0f2fa39b1e3e5dfcec75e34696d08530020fd82b
Claire Bergey <clairebergey@gmail.com>
2022-08-09 16:43:29 -0500

    fix port

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ if(argv.gameport) {
   gameport = argv.gameport;
   console.log('using port ' + gameport);
 } else {
-  gameport = 8887;
+  gameport = process.env.PORT;
   console.log('no gameport specified: using 8887\nUse the --gameport flag to change');
 }
 
